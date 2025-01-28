c4637e42284186742c035d4972cd5a6b60f4bf60
arifulislamk <ariful18374@gmail.com>
2025-01-28 15:26:35 +0600

    crate a table in mysql

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const sequelize = new Sequelize('User', 'root', 'root',
     }
 );
 
+const User = sequelize.define('user', {
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+    },
+    age: {
+        type: Sequelize.DataTypes.INTEGER,
+        defaultValue: 21
+    }
+},
+{
+    freezeTableName: true,
+    tableName: 'table',
+    
+})
+
+User.sync().then((data)=> {
+    console.log('Table created successfully');
+}).catch((err)=> {
+    console.error('Unable to connect to the database:', err);
+})
+console.log('Table created successfully', sequelize.model.User);
 sequelize.authenticate().then( ()=> {
     console.log('Connection has been established successfully.');
 }).catch( (error)=> {
