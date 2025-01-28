0f0792f23a9aa76b3ea4f5c88bfa1dd7724ee7ab
arifulislamk <ariful18374@gmail.com>
2025-01-28 21:00:44 +0600

    insert multiple data

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('User', 'root', 'root', 
-    {
-        dialect:'mysql',
-    }
-);
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("User", "root", "root", {
+  dialect: "mysql",
+});
 
-const User = sequelize.define('user', {
+const User = sequelize.define(
+  "user",
+  {
     user_id: {
-        type: Sequelize.DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: Sequelize.DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     username: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false,
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
     },
     password: {
-        type: Sequelize.DataTypes.STRING,
+      type: Sequelize.DataTypes.STRING,
     },
     age: {
-        type: Sequelize.DataTypes.INTEGER,
-        defaultValue: 21
-    }
-},
-{
+      type: Sequelize.DataTypes.INTEGER,
+      defaultValue: 21,
+    },
+  },
+  {
     freezeTableName: true,
-    tableName: 'table',
+    tableName: "table",
+  }
+);
 
-})
-
-User.sync({alert : true}).then((data)=> {
-    const user = User.build({username: "arif", password: "123"})
-    user.age = 45 ;
-    return user.save({field : ['age']})
-}).then((data) => {
-    console.log('User added data in database',)
-}).catch((err)=> {
-    console.error('Unable to connect to the database:', err);
-})
-console.log('Table created successfully', sequelize.model.User);
-sequelize.authenticate().then( ()=> {
-    console.log('Connection has been established successfully.');
-}).catch( (error)=> {
-    console.error('Unable to connect to the database:', error);
-})
-console.log('Connection has been established successfully')
+User.sync({ alert: true })
+  .then(() => {
+    return User.bulkCreate([
+      { username: "dany", password: "password123" },
+      { username: "Jane Doe", password: "password456" },
+      { username: "Alice Johnson", password: "password789" },
+    ]);
+  }).then((data) => {
+    console.log("User updated data in database");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+console.log("Table created successfully", sequelize.model.User);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+console.log("Connection has been established successfully");
