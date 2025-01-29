092e3f1bc56ce040e4c4bb9502c7195e5fa2003a
arifulislamk <ariful18374@gmail.com>
2025-01-29 22:52:40 +0600

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const sequelize = new Sequelize("User", "root", "root", {
   dialect: "mysql",
 });
 
+// create the database table
 const User = sequelize.define(
   "user",
   {
@@ -28,32 +29,33 @@ const User = sequelize.define(
     tableName: "table",
   }
 );
+
+//insert data in database
+// User.sync({ alter: true })
+//   .then(() => {
+//     return User.bulkCreate([
+//       { username: "dany", password: "password123" },
+//       { username: "Jane Doe", password: "password456" },
+//       { username: "Alice Johnson", password: "password789" },
+//     ]);
+//   }).then((data) => {
+//     console.log("User updated data in database");
+//   })
+//   .catch((err) => {
+//     console.error("Unable to connect to the database:", err);
+//   });
+
+
+  //findall tables data form database
 User.sync({alter:true})
 .then(() =>{
-  return User.findAll() ;
+  return User.findAll({ limit: 5}) ;
 }).then((data)=> {
   data.forEach((element)=> {
     console.log(element.toJSON());
   })
 })
-// insert data in database
-User.sync({ alter: true })
-  .then(() => {
-    return User.bulkCreate([
-      { username: "dany", password: "password123" },
-      { username: "Jane Doe", password: "password456" },
-      { username: "Alice Johnson", password: "password789" },
-    ]);
-  }).then((users) => {
-    const user = users[0];
-    user.age = 65
-    return user.save({fields : ['age']})
-  }).then((data) => {
-    console.log("User updated data in database");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+
 sequelize
   .authenticate()
   .then(() => {
